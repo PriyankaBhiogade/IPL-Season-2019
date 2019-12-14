6fd892eba1844c85ad5b4ff672fa88c630e38a9b
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-12-14 16:00:04 +0530

    Add: Test for throwing exception When delimiter  issue

--- src/test/java/com/ipl2019/CricketLeagueAnalyserTest.java
+++ src/test/java/com/ipl2019/CricketLeagueAnalyserTest.java
@@ -5,12 +5,12 @@ import org.junit.Test;
 
 public class CricketLeagueAnalyserTest {
 
-    private static final String IPL_RUNS_CSV_FILE_PATH = "IPL2019FactsheetMostRuns.csv";
+    private static final String IPL_RUNS_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
     private static final String IPL_RUNS_CSV_FILE_PATH_WITH_WRONG_FILE = "IPL2019FactsheetMostRuns123.csv";
-    private static final String IPL_RUNS_CSV_FILE_PATH_WITH_DELIMITER_ERR = "IPL2019FactsheetMostRunsWithDelimiterError.csv";
-    private static final String IPL_RUNS_CSV_FILE_PATH_WITH_HEADER_ERR = "IPL2019FactsheetMostRunsWithHeaderError.csv";
-    private static final String EMPTY_FILE_PATH = "EmptyFile.csv";
-    private static final String IPL_WKTS_CSV_FILE_PATH = "IPL2019FactsheetMostWkts.csv";
+    private static final String IPL_RUNS_CSV_FILE_PATH_WITH_DELIMITER_ERR = "./src/test/resources/IPL2019FactsheetMostRunsWithDelimiterErr.csv";
+    private static final String IPL_RUNS_CSV_FILE_PATH_WITH_HEADER_ERR = "./src/test/resources/IPL2019FactsheetMostRunsWithHeaderError.csv";
+    private static final String EMPTY_FILE_PATH = "./src/test/resources/EmptyFile.csv";
+    private static final String IPL_WKTS_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostWkts.csv";
 
     @Test
     public void givenCricketLeagueRunsCsvFile_WhenCorrectRecord_ShouldReturnRecordCount() {
@@ -24,11 +24,22 @@ public class CricketLeagueAnalyserTest {
 
     @Test
     public void givenCricketLeagueRunsCsvFile_WhenFileIsWrong_ShouldThrowingException() {
-        CricketLeagueAnalyser iplAnalyser = new CricketLeagueAnalyser();
         try {
+            CricketLeagueAnalyser iplAnalyser = new CricketLeagueAnalyser();
             iplAnalyser.loadILPData(IPL_RUNS_CSV_FILE_PATH_WITH_WRONG_FILE);
         } catch (CricketLeagueAnalyserException e) {
             Assert.assertEquals(CricketLeagueAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
         }
     }
+
+    @Test
+    public void givenCricketLeagueRunsCsvFile_WhenDelimiterIssue_ShouldThrowingException() {
+        try {
+            CricketLeagueAnalyser iplAnalyser = new CricketLeagueAnalyser();
+            iplAnalyser.loadILPData(IPL_RUNS_CSV_FILE_PATH_WITH_DELIMITER_ERR);
+        } catch (CricketLeagueAnalyserException e) {
+            Assert.assertEquals(CricketLeagueAnalyserException.ExceptionType.SOME_ISSUE_IN_FILE, e.type);
+        }
+    }
+
 }
