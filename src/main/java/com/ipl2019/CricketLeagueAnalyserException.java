bbfe7128d9c7a05e101976842afab00f0b984c1b
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-12-14 14:50:35 +0530

    Add: Add Required functionality

--- src/main/java/com/ipl2019/CricketLeagueAnalyserException.java
+++ src/main/java/com/ipl2019/CricketLeagueAnalyserException.java
@@ -2,4 +2,14 @@ package com.ipl2019;
 
 public class CricketLeagueAnalyserException extends Exception {
 
+    ExceptionType type;
+
+    public enum ExceptionType {
+        CENSUS_FILE_PROBLEM,ERROR_FROM_CSV_BUILDER
+    }
+
+    public CricketLeagueAnalyserException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
 }
