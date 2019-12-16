a2119273bc97053f760098f5b6c39c1f07707d4f
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-12-16 11:10:50 +0530

    Add: Class Which contain Logic of Comaparing data

--- src/main/java/com/ipl2019/SortMethod.java
+++ src/main/java/com/ipl2019/SortMethod.java
@@ -1,4 +1,11 @@
 package com.ipl2019;
 
-public class SortMethod {
+import java.util.Comparator;
+
+public class SortMethod implements Comparator<IPLRunsDAO> {
+
+    @Override
+    public int compare(IPLRunsDAO mostRun1, IPLRunsDAO mostRun2) {
+        return ((mostRun1.six * 6) + (mostRun1.four * 4)) - ((mostRun2.six * 6) + (mostRun2.four * 4));
+    }
 }
