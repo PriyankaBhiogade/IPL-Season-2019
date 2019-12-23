52ee8d27f319f84e0537bc0eee7469fbb6e2d9ea
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-12-23 12:46:42 +0530

    Added: Code for Merging Bowling & Batting File

---
+++ src/main/java/com/ipl2019/MergeDataAdapter.java
@@ -0,0 +1,17 @@
+package com.ipl2019;
+
+import java.util.Map;
+import java.util.stream.StreamSupport;
+
+public class MergeDataAdapter extends IPLLoaderAdapter {
+
+    @Override
+    public Map<String, IPLDAO> loadData(String... csvFilePath) throws CricketLeagueAnalyserException {
+        Map<String, IPLDAO> battingMap = super.loadData(IPLBatsmanCSV.class, csvFilePath[0]);
+        Map<String, IPLDAO> bowlingMap = super.loadData(IPLBowlerCSV.class, csvFilePath[1]);
+        bowlingMap.values().stream()
+                .filter(iplRanData -> battingMap.get(iplRanData.player) != null)
+                .forEach(iplData -> battingMap.get(iplData.player).bowlingAvg = iplData.bowlingAvg);
+        return battingMap;
+    }
+}
