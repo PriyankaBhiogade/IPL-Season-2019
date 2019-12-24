f7368a211226f1f4b40d74e2bffef1639998694e
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-12-24 18:40:30 +0530

    Added:Csv Builder Dependency

--- src/main/java/com/ipl2019/IPLDAO.java
+++ src/main/java/com/ipl2019/IPLDAO.java
@@ -15,15 +15,25 @@ public class IPLDAO {
     public int totalWickets;
 
 
-    public IPLDAO(String player, int run, double avg, double strikeRate) {
+    public IPLDAO(String player, int run, double battingAvg, double batsmanStrikeRate, double economy, int six, int four) {
         this.player = player;
         this.run = run;
-        this.battingAvg = avg;
-        this.bowlingAvg = avg;
-        this.batsmanStrikeRate = strikeRate;
-        this.bowlerStrikeRate = strikeRate;
+        this.battingAvg = battingAvg;
+        this.batsmanStrikeRate = batsmanStrikeRate;
+        this.six = six;
+        this.four = four;
+        this.economy = economy;
     }
 
+    public IPLDAO(String player, double bowlerStrikeRate, double bowlingAvg, double economy, int wickets_4, int wickets_5, int totalWickets) {
+        this.player = player;
+        this.bowlerStrikeRate = bowlerStrikeRate;
+        this.bowlingAvg = bowlingAvg;
+        this.economy = economy;
+        this.wickets_4 = wickets_4;
+        this.wickets_5 = wickets_5;
+        this.totalWickets = totalWickets;
+    }
 
     public IPLDAO(IPLBatsmanCSV runCSV) {
         this.player = runCSV.player;
